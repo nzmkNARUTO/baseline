ef168df2346f50bc2d4b5cd0b324e4fe7c52ea06
nzmkNARUTO <nzmkNARUTO@outlook.com>
2024-12-18 11:23:45 +0000

    add algorithm to server end

--- fed_baselines/server_fedavg_plus.py
+++ fed_baselines/server_fedavg_plus.py
@@ -1,32 +1,21 @@
-from utils.fed_utils import init_model
 from fed_baselines.server_base import FedServer
-import copy
+from utils.models import *
 import torch
 import math
-import numpy as np
-from torch import nn
+import copy
 from torch.utils.data import DataLoader, random_split
+from utils.fed_utils import assign_dataset, init_model
 
 
-class ScaffoldPlusServer(FedServer):
-
+class FedAvgPlusServer(FedServer):
     def __init__(self, client_list, dataset_id, model_name, len_class, num_round):
         super().__init__(client_list, dataset_id, model_name)
-        # server control variate
-        self.scv = init_model(
-            model_name=self.model_name,
-            num_class=self._num_class,
-            image_channel=self._image_channel,
-        )
-        # Dict of all client control variates
-        self.client_ccv_state = {}
         self.len_class = len_class
         self.num_round = num_round
         self.all_clients_data_distribution = {}
         self.selected_clients_data_distribution = {}
 
     def calculate_data_distribution(self):
-
         for name in self.selected_clients:
             self.selected_clients_data_distribution[name] = (
                 self.all_clients_data_distribution[name]
@@ -42,6 +31,7 @@ class ScaffoldPlusServer(FedServer):
                     self.selected_clients_data_distribution[name][i]
                     / self.client_n_data[name]
                 )
+        pass
 
     def analysis_sensitivity(self):
         self.calculate_data_distribution()
@@ -86,7 +76,7 @@ class ScaffoldPlusServer(FedServer):
         for name in self.selected_clients:
             mask = {}
             for i in range(self.len_class):
-                x = 0.8
+                x = 0.5
                 ratio = self.selected_clients_data_distribution[name][i] * (1 - x) + x
                 # ratio = 1
                 mask[i] = self.mask(sensitivities[i], self.toOne(ratio))
@@ -105,48 +95,32 @@ class ScaffoldPlusServer(FedServer):
 
     def agg(self):
         """
-        Server aggregates normalized models from connected clients using SCAFFOLD
-        :return: Updated global model after aggregation, Averaged loss value, Number of the local data points, server control variate
+        Server aggregates models from connected clients.
+        :return: model_state: Updated global model after aggregation
+        :return: avg_loss: Averaged loss value
+        :return: n_data: Number of the local data points
         """
         client_num = len(self.selected_clients)
         if client_num == 0 or self.n_data == 0:
             return self.model.state_dict(), 0, 0
-
-        self.scv.to(self._device)
-        self.model.to(self._device)
+        # self.calculate_data_distribution()
         model_state = self.model.state_dict()
-        new_scv_state = copy.deepcopy(model_state)
         avg_loss = 0
 
-        # analysis the sensitivity of each class
+        # # analysis the sensitivity of each class
         sensitivities = self.analysis_sensitivity()
 
         # calculate the gradient
         delta_state = self.calculate_gradient(model_state)
 
-        # reweight the gradient
+        # # reweight the gradient
         delta_state = self.reweight_gradient(sensitivities, delta_state)
 
-        # print('number of selected clients in Cloud: ' + str(client_num))
+        # Aggregate the local updated models from selected clients
         for i, name in enumerate(self.selected_clients):
             if name not in self.client_state:
                 continue
             for key in self.client_state[name]:
-
-                if i == 0:
-                    new_scv_state[key] = (
-                        self.client_ccv_state[name][key]
-                        * self.client_n_data[name]
-                        / self.n_data
-                    )
-                else:
-                    new_scv_state[key] = (
-                        new_scv_state[key]
-                        + self.client_ccv_state[name][key]
-                        * self.client_n_data[name]
-                        / self.n_data
-                    )
-
                 model_state[key] += (
                     delta_state[name][key] * self.client_n_data[name] / self.n_data
                 )
@@ -156,13 +130,12 @@ class ScaffoldPlusServer(FedServer):
                 + self.client_loss[name] * self.client_n_data[name] / self.n_data
             )
 
-        scv_state = self.scv.state_dict()
-
+        # Server load the aggregated model as the global model
         self.model.load_state_dict(model_state)
-        self.scv.load_state_dict(new_scv_state)
         self.round = self.round + 1
+        n_data = self.n_data
 
-        return model_state, avg_loss, self.n_data, scv_state
+        return model_state, avg_loss, n_data
 
     def load_testset(self, testset):
         """
@@ -195,43 +168,32 @@ class ScaffoldPlusServer(FedServer):
 
     def toUniform(self, data_distribution):
         # return data_distribution
-        average = 1 / self._num_class
+        average = 1 / self.len_class
         return (
             data_distribution
             + (average - data_distribution) / self.num_round * self.round
         )
 
     def toOne(self, data_distribution):
+        # return data_distribution
         return data_distribution + (1 - data_distribution) / self.num_round * self.round
 
-    def rec(
-        self,
-        name,
-        state_dict,
-        n_data,
-        loss,
-        ccv_state,
-        client_id,
-        client_data_distribution,
-    ):
+    def rec(self, name, state_dict, n_data, loss, client_id, client_data_distribution):
         """
         Server receives the local updates from the connected client k.
         :param name: Name of client k
         :param state_dict: Model dict from the client k
         :param n_data: Number of local data points in the client k
         :param loss: Loss of local training in the client k
-        :param ccv_state: Normalization coefficient
         """
         self.n_data = self.n_data + n_data
         self.client_state[name] = {}
         self.client_n_data[name] = {}
-        self.client_ccv_state[name] = {}
 
         self.client_state[name].update(state_dict)
         self.client_n_data[name] = n_data
         self.client_loss[name] = {}
         self.client_loss[name] = loss
-        self.client_ccv_state[name].update(ccv_state)
         self.all_clients_data_distribution[client_id] = client_data_distribution
 
     def flush(self):
@@ -242,5 +204,4 @@ class ScaffoldPlusServer(FedServer):
         self.client_state = {}
         self.client_n_data = {}
         self.client_loss = {}
-        self.client_ccv_state = {}
         self.selected_clients_data_distribution = {}
