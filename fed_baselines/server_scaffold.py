216dbe2059606f08d493b34ea9ae975842a5be51
nzmkNARUTO <nzmkNARUTO@outlook.com>
2024-12-18 11:24:01 +0000

    format code

--- fed_baselines/server_scaffold.py
+++ fed_baselines/server_scaffold.py
@@ -1,15 +1,32 @@
 from utils.fed_utils import init_model
 from fed_baselines.server_base import FedServer
 import copy
+from torch.utils.data import DataLoader, random_split
 
 
 class ScaffoldServer(FedServer):
     def __init__(self, client_list, dataset_id, model_name):
         super().__init__(client_list, dataset_id, model_name)
         # server control variate
-        self.scv = init_model(model_name=self.model_name, num_class=self._num_class, image_channel=self._image_channel)
+        self.scv = init_model(
+            model_name=self.model_name,
+            num_class=self._num_class,
+            image_channel=self._image_channel,
+        )
         # Dict of all client control variates
         self.client_ccv_state = {}
+
+    def calculate_gradient(self, model_state):
+        # calculate the gradient
+        delta_state = {}
+        for name in self.selected_clients:
+            delta_state[name] = self.model.state_dict()
+        for name in self.selected_clients:
+            if name not in self.client_state:
+                continue
+            for key in self.client_state[name]:
+                delta_state[name][key] = self.client_state[name][key] - model_state[key]
+        return delta_state
 
     def agg(self):
         """
@@ -25,6 +42,10 @@ class ScaffoldServer(FedServer):
         model_state = self.model.state_dict()
         new_scv_state = copy.deepcopy(model_state)
         avg_loss = 0
+
+        # calculate the gradient
+        delta_state = self.calculate_gradient(model_state)
+
         # print('number of selected clients in Cloud: ' + str(client_num))
         for i, name in enumerate(self.selected_clients):
             if name not in self.client_state:
@@ -32,16 +53,39 @@ class ScaffoldServer(FedServer):
             for key in self.client_state[name]:
 
                 if i == 0:
-                    model_state[key] = self.client_state[name][key] * self.client_n_data[name] / self.n_data
-                    new_scv_state[key] = self.client_ccv_state[name][key] * self.client_n_data[name] / self.n_data
+                    # model_state[key] = (
+                    #     self.client_state[name][key]
+                    #     * self.client_n_data[name]
+                    #     / self.n_data
+                    # )
+                    new_scv_state[key] = (
+                        self.client_ccv_state[name][key]
+                        * self.client_n_data[name]
+                        / self.n_data
+                    )
 
                 else:
-                    model_state[key] = model_state[key] + self.client_state[name][key] * self.client_n_data[
-                        name] / self.n_data
-                    new_scv_state[key] = new_scv_state[key] + self.client_ccv_state[name][key] * self.client_n_data[
-                        name] / self.n_data
+                    # model_state[key] = (
+                    #     model_state[key]
+                    #     + self.client_state[name][key]
+                    #     * self.client_n_data[name]
+                    #     / self.n_data
+                    # )
+                    new_scv_state[key] = (
+                        new_scv_state[key]
+                        + self.client_ccv_state[name][key]
+                        * self.client_n_data[name]
+                        / self.n_data
+                    )
 
-            avg_loss = avg_loss + self.client_loss[name] * self.client_n_data[name] / self.n_data
+                model_state[key] += (
+                    delta_state[name][key] * self.client_n_data[name] / self.n_data
+                )
+
+            avg_loss = (
+                avg_loss
+                + self.client_loss[name] * self.client_n_data[name] / self.n_data
+            )
 
         scv_state = self.scv.state_dict()
 
@@ -71,7 +115,6 @@ class ScaffoldServer(FedServer):
         self.client_loss[name] = loss
         self.client_ccv_state[name].update(ccv_state)
 
-
     def flush(self):
         """
         Flushing the client information in the server
@@ -81,3 +124,14 @@ class ScaffoldServer(FedServer):
         self.client_n_data = {}
         self.client_loss = {}
         self.client_ccv_state = {}
+
+    def load_testset(self, testset):
+        """
+        Server loads the test dataset.
+        :param data: Dataset for testing.
+        """
+        test_size = int(len(testset) * 0.9)
+        analysis_size = len(testset) - test_size
+        self.testset, self.analysisset = random_split(
+            testset, [test_size, analysis_size]
+        )
