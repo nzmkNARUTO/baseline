d571b5c1e5cd05f7a46e2937c30e41f6647b5111
nzmkNARUTO <nzmkNARUTO@outlook.com>
2025-03-10 06:39:08 +0000

    add x

--- run.py
+++ run.py
@@ -51,9 +51,10 @@ def run(config):
         + str(config["system"]["divide_method"])
     )
     if config["system"]["divide_method"] == "Dirichlet":
-        file_address += "_a=" + str(config["system"]["alpha"]) + ".yaml"
+        file_address += "_a=" + str(config["system"]["alpha"])
     else:
-        file_address += "_n=" + str(config["system"]["num_local_class"]) + ".yaml"
+        file_address += "_n=" + str(config["system"]["num_local_class"])
+    file_address += "_x=" + str(config["system"]["x"]) + ".yaml"
     with open(file_address, "w") as f:
         yaml.dump(config, f)
     print(f"python fl_main.py --config {file_address}")
