8b66efd211845c536ae494a8b56d2bb2ae69b018
nzmkNARUTO <nzmkNARUTO@outlook.com>
2025-03-27 07:23:07 +0000

    load dataset

--- test.py
+++ test.py
@@ -7,3 +7,7 @@ import torchvision.transforms as transforms
 from preprocessing.baselines_dataloader import load_data
 
 load_data("EMNIST")
+load_data("FashionMNIST")
+load_data("MNIST")
+load_data("CIFAR10")
+load_data("CIFAR100")
