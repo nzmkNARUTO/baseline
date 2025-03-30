6268cdf2880ec2eaba45a9db93e1c209dc3dcbfe
nzmkNARUTO <nzmkNARUTO@outlook.com>
2025-03-30 06:03:50 +0000

    revert

--- run.py
+++ run.py
@@ -66,7 +66,7 @@ def run(config):
         + "_x="
         + str(config["system"]["x"]),
     )
-    if not os.path.exists(config["system"]["res_root"]):
+    if not os.path.exists(file_path):
         file_address = (
             "config/"
             + str(config["client"]["fed_algo"])
